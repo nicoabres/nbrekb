8d5097a85adcddf037c2b69312e4122b07b8fc5d
Nico Bressler <nico@nbre.dev>
2020-05-30 21:06:22 -0700

    Added agent list

--- routes/agents.js
+++ routes/agents.js
@@ -12,159 +12,182 @@ var Agent = require('../models/agent');
 
 /* GET agents page. */
 router.get('/', function(req, res, next) {
-    Agent.aggregate([
+    if (!req.query.id) {
+      Agent.aggregate([
         {
-            '$match': {
-                id: parseInt(req.query.id)
-            }
+          '$lookup': {
+              'from': 'corporations',
+              'localField': 'corporationID',
+              'foreignField': 'id',
+              'as': 'corporation'
+          }
         },
         {
-            '$lookup': {
-                'from': 'corporations',
-                'localField': 'corporationID',
-                'foreignField': 'id',
-                'as': 'corporation'
-            }
-        },
-        {
-            '$unwind': '$corporation'
+          '$unwind': '$corporation'
         }
-    ])
-    .exec(function (error, agentInfo) {
+      ])
+      .exec(function (error, agents) {
         if (error) {
-            console.error(log)
+          console.error(error)
         } else {
-            var agentInfo = agentInfo[0]
-            Kill.aggregate([
-                {
-                    '$match': {
-                        'attackers._embedded.agent.id': parseInt(agentInfo.id)
-                    }
-                },
-                {
-                    '$lookup': {
-                      'from': 'agents',
-                      'localField': 'agentID',
-                      'foreignField': 'id',
-                      'as': 'agent'
-                    }
-                  },
-                  {
-                    '$unwind': '$agent'
-                  },
-                  {
-                    '$lookup': {
-                      'from': 'corporations',
-                      'localField': 'corporationID',
-                      'foreignField': 'id',
-                      'as': 'corporation'
-                    }
-                  },
-                  {
-                    '$unwind': '$corporation'
-                  },
-                  {
-                    '$lookup': {
-                      'from': 'robots',
-                      'localField': 'robotID',
-                      'foreignField': 'id',
-                      'as': 'robot'
-                    }
-                  },
-                  {
-                    '$unwind': "$robot"
-                  },
-                  {
-                    '$lookup': {
-                      'from': 'zones',
-                      'localField': 'zoneID',
-                      'foreignField': 'id',
-                      'as': 'zone'
-                    }
-                  },
-                  {
-                    '$unwind': "$zone"
-                  }
-            ])
-            .sort({date: 'desc'})
-            .exec(function (error, agentKills) {
-                if (error) {
-                    console.error(error)
-                } else {
-                    var agentKillingBlows = []
-                    agentKills.forEach(kill => {
-                        kill.attackers.forEach(attacker => {
-                            if (attacker.hasKillingBlow) {
-                                if (agentInfo.id == attacker._embedded.agent.id) {
-                                    agentKillingBlows.push(kill)
-                                }
-                            }
-                        })
-                    })
-                    
-                    Kill.aggregate([
-                        {
-                            '$match': {
-                                'agentID': agentInfo.id
-                            }
-                        },
-                        {
-                        '$lookup': {
-                            'from': 'agents',
-                            'localField': 'agentID',
-                            'foreignField': 'id',
-                            'as': 'agent'
-                        }
-                        },
-                        {
-                            '$unwind': "$agent"
-                        },
-                        {
-                            '$lookup': {
-                                'from': 'corporations',
-                                'localField': 'corporationID',
-                                'foreignField': 'id',
-                                'as': 'corporation'
-                            }
-                        },
-                        {
-                            '$unwind': "$corporation"
-                        },
-                        {
-                            '$lookup': {
-                                'from': 'robots',
-                                'localField': 'robotID',
-                                'foreignField': 'id',
-                                'as': 'robot'
-                            }
-                        },
-                        {
-                            '$unwind': "$robot"
-                        },
-                        {
-                            '$lookup': {
-                                'from': 'zones',
-                                'localField': 'zoneID',
-                                'foreignField': 'id',
-                                'as': 'zone'
-                            }
-                        },
-                        {
-                            '$unwind': "$zone"
-                        }
-                    ])
-                    .sort({date: 'desc'})
-                    .exec(function (error, agentLosses) {
-                        if (error) {
-                            console.error(error)
-                        } else {
-                            res.render('agent', {title: agentInfo.name + ' | Agent | nbreKB', agentInfo: agentInfo, agentKillingBlows: agentKillingBlows, agentLosses: agentLosses, moment:moment })
-                        }
-                    })
-                }
-            })
+          res.render('agent_list', {title: 'Agents | nbreKB', agents: agents})
         }
-    })
+      })
+    } else {
+      Agent.aggregate([
+          {
+              '$match': {
+                  id: parseInt(req.query.id)
+              }
+          },
+          {
+              '$lookup': {
+                  'from': 'corporations',
+                  'localField': 'corporationID',
+                  'foreignField': 'id',
+                  'as': 'corporation'
+              }
+          },
+          {
+              '$unwind': '$corporation'
+          }
+      ])
+      .exec(function (error, agentInfo) {
+          if (error) {
+              console.error(log)
+          } else {
+              var agentInfo = agentInfo[0]
+              Kill.aggregate([
+                  {
+                      '$match': {
+                          'attackers._embedded.agent.id': parseInt(agentInfo.id)
+                      }
+                  },
+                  {
+                      '$lookup': {
+                        'from': 'agents',
+                        'localField': 'agentID',
+                        'foreignField': 'id',
+                        'as': 'agent'
+                      }
+                    },
+                    {
+                      '$unwind': '$agent'
+                    },
+                    {
+                      '$lookup': {
+                        'from': 'corporations',
+                        'localField': 'corporationID',
+                        'foreignField': 'id',
+                        'as': 'corporation'
+                      }
+                    },
+                    {
+                      '$unwind': '$corporation'
+                    },
+                    {
+                      '$lookup': {
+                        'from': 'robots',
+                        'localField': 'robotID',
+                        'foreignField': 'id',
+                        'as': 'robot'
+                      }
+                    },
+                    {
+                      '$unwind': "$robot"
+                    },
+                    {
+                      '$lookup': {
+                        'from': 'zones',
+                        'localField': 'zoneID',
+                        'foreignField': 'id',
+                        'as': 'zone'
+                      }
+                    },
+                    {
+                      '$unwind': "$zone"
+                    }
+              ])
+              .sort({date: 'desc'})
+              .exec(function (error, agentKills) {
+                  if (error) {
+                      console.error(error)
+                  } else {
+                      var agentKillingBlows = []
+                      agentKills.forEach(kill => {
+                          kill.attackers.forEach(attacker => {
+                              if (attacker.hasKillingBlow) {
+                                  if (agentInfo.id == attacker._embedded.agent.id) {
+                                      agentKillingBlows.push(kill)
+                                  }
+                              }
+                          })
+                      })
+                      
+                      Kill.aggregate([
+                          {
+                              '$match': {
+                                  'agentID': agentInfo.id
+                              }
+                          },
+                          {
+                          '$lookup': {
+                              'from': 'agents',
+                              'localField': 'agentID',
+                              'foreignField': 'id',
+                              'as': 'agent'
+                          }
+                          },
+                          {
+                              '$unwind': "$agent"
+                          },
+                          {
+                              '$lookup': {
+                                  'from': 'corporations',
+                                  'localField': 'corporationID',
+                                  'foreignField': 'id',
+                                  'as': 'corporation'
+                              }
+                          },
+                          {
+                              '$unwind': "$corporation"
+                          },
+                          {
+                              '$lookup': {
+                                  'from': 'robots',
+                                  'localField': 'robotID',
+                                  'foreignField': 'id',
+                                  'as': 'robot'
+                              }
+                          },
+                          {
+                              '$unwind': "$robot"
+                          },
+                          {
+                              '$lookup': {
+                                  'from': 'zones',
+                                  'localField': 'zoneID',
+                                  'foreignField': 'id',
+                                  'as': 'zone'
+                              }
+                          },
+                          {
+                              '$unwind': "$zone"
+                          }
+                      ])
+                      .sort({date: 'desc'})
+                      .exec(function (error, agentLosses) {
+                          if (error) {
+                              console.error(error)
+                          } else {
+                              res.render('agent', {title: agentInfo.name + ' | Agent | nbreKB', agentInfo: agentInfo, agentKillingBlows: agentKillingBlows, agentLosses: agentLosses, moment:moment })
+                          }
+                      })
+                  }
+              })
+          }
+      })
+    }
 });
 
 module.exports = router;
