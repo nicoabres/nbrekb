bc07ea7af3ca22672b281aa7784ae2cce413707e
Nico Bressler <nico@nbre.dev>
2020-05-30 19:57:55 -0700

    Added zone kill functionality

--- app.js
+++ app.js
@@ -376,6 +376,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var corporationsRouter = require('./routes/corporations');
 var agentsRouter = require('./routes/agents');
+var zonesRouter = require('./routes/zones');
 
 var app = express();
 
@@ -393,6 +394,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/corporations', corporationsRouter);
 app.use('/agents', agentsRouter);
+app.use('/zones', zonesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/zones.js
+++ routes/zones.js
@@ -6,10 +6,85 @@ var router = express.Router();
 var moment = require('moment');
 
 // Get our models
-var Corporation = require('../models/corporation');
+var Zone = require('../models/zone');
 var Kill = require('../models/kill');
 
 /* GET corporations listing. */
 router.get('/', function(req, res, next) {
-    res.render('zone', { title: 'Zone | nbreKB' });
+	Zone.aggregate([
+		{
+			'$match': {
+				'id': parseInt(req.query.id)
+			}
+		}
+	])
+	.exec(function (error, zoneInfo) {
+		if (error) {
+			console.error(error)
+		} else {
+      var zoneInfo = zoneInfo[0]
+			Kill.aggregate([
+				{
+					'$match': {
+						'zoneID': parseInt(req.query.id)
+					}
+				},
+				{
+					'$lookup': {
+                      'from': 'agents',
+                      'localField': 'agentID',
+                      'foreignField': 'id',
+                      'as': 'agent'
+                    }
+				},
+				{
+                    '$unwind': '$agent'
+              	},
+              	{
+                    '$lookup': {
+                      'from': 'corporations',
+                      'localField': 'corporationID',
+                      'foreignField': 'id',
+                      'as': 'corporation'
+                    }
+              	},
+              	{
+                    '$unwind': '$corporation'
+              	},
+              	{
+                    '$lookup': {
+                      'from': 'robots',
+                      'localField': 'robotID',
+                      'foreignField': 'id',
+                      'as': 'robot'
+                    }
+              	},
+              	{
+                    '$unwind': "$robot"
+              	},
+              	{
+                    '$lookup': {
+                      'from': 'zones',
+                      'localField': 'zoneID',
+                      'foreignField': 'id',
+                      'as': 'zone'
+                    }
+              	},
+              	{
+                	'$unwind': "$zone"
+              	}
+			])
+			.sort({date: 'desc'})
+			.exec(function (error, zoneKills) {
+				if (error) {
+					console.error(error)
+				} else {
+          console.log(zoneInfo)
+					res.render('zone', { title: zoneInfo.name + ' | Zone | nbreKB', zoneKills: zoneKills, zoneInfo: zoneInfo, moment: moment });
+				}
+			})
+		}
+	})
 })
+
+module.exports = router;
